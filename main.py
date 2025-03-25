65d831b5d22a9f42511059f5216867adcfd10a0c
Jason Zeng <101027720+IcedLimes@users.noreply.github.com>
2025-03-25 13:44:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+trivia = [
+    {
+     "questions": "What Country is Paris located in?"
+     "options:": ["United Kingdom", "United States", "Germany", "France"]
+     "answer": "France"
+    },
+    {
+     "questions": "What is the captial of Romania?"
+     "options:": ["Bucharest", "Sibiu", "Dej", "Oradea"]
+     "answer": "Bucharest"
+    },
+    {
+     "questions": "What continent is Ghana located in?"
+     "options:": ["North America", "South America", "Asia", "Africa"]
+     "answer": "Africa"
+    },
+    {
+     "questions": "What country does France share its longest land borderr with?"
+     "options:": ["United States", "Germany", "Brazil", "Africa"]
+     "answer": "Africa"
+    }
+    {
+     "questions": "What is considered the country that never gets dark?"
+     "options:": ["Norway", "Swede"
+    }
+,
+    }
+    {
+     "questions": "What is a city in brazil?"
+     "options:": ["Salvador", "Puebla", "Monterrey", "Bilbao"]
+     "answer": "Salvador"
+    },
+]
